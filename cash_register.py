1023db0a588ed051d729f9617d5f1de20c09bed5
Hank <hlee1@berklee.edu>
2026-09-24 18:09:03 -0400

    completed python day 1

---
+++ cash_register.py
@@ -0,0 +1,11 @@
+,,,
+Task: Calculate the sales tax
+Name: calc_sales_tax
+Input: order_total, tax_rate
+Side Effect: no 
+Return: tax
+,,,
+
+def calc_slaes_tax(order_total, tax_rate):
+# worry about this later
+    return tax 
---
+++ demo.py
@@ -0,0 +1,34 @@
+print("Hello, world!")
+
+# Comments! Human readable. Not code. Python ignores.
+# name: make_batter
+# input: nothing
+# side effects: none
+# return: batter
+
+def make_batter():
+    # we will worry about this later
+    return batter 
+
+
+# Task: Takes in batter and the number of pancakes request, and grills them.
+# Name: batter, num_of _pc
+# Side Effects: No
+# Return: plate_of_pc
+
+def grill_pancakes(batter, number_of_pc):
+    # we will worry about this later
+    return plate of pc 
+   
+ ,,,
+Takes: deliver plates to customers
+Name: deliver_plate
+Input: plate
+Side Effect: say "enjoy your meal"
+Return: no 
+,,,
+
+def deliver_plate(plate):
+    # worry later
+    print("Enjoy your meal! ")
+    return None
