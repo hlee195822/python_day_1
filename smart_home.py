47b14d589c747c4bdb57b7336aea98dc17cb78be
Hank <hlee1@berklee.edu>
2026-09-24 18:14:29 -0400

    completed python day 1

--- smart_home.py
+++ smart_home.py
@@ -1,21 +1,24 @@
-,,,
+'''
+
 Task: comvert c to finally
 Name: c_to_f
 Input: degrees_c
 Side Effects: no 
 Return: degrees_f
-,,,
+'''
 def c_to_f(degrees_c):
     degrees_f =(degrees_c *9/5) + 32
     return degrees_f
 
-,,,
+'''
+
 Task: tell the user the current temperature
 Name: print_temp
 Input: temp_in_f
 Side Effects: "the temperature is: x"
 Return: no 
-,,,
+'''
+
 
 def print_temp(temp_in_f):
     return None
